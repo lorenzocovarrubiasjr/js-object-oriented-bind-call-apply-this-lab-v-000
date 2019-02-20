7f53943468217b1b228f53a5849ec9b8eb393c23
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-20 04:26:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//Your code here
+function justInvoke(fn) {
+   return x = fn();
+}
+
+ function setThisWithCall(fn, thisValue, arg) {
+   return x = fn.call(thisValue, arg)
+ }
+
+function setThisWithApply(fn, thisValue, arg) {
+  return x = fn.apply(thisValue, arg)
+}
+
+function returnNewFunctionOf(fn, thisValue) {
+  const copyFn = fn.bind(thisValue);
+  return copyFn
+}
